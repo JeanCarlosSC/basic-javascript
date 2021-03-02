f93c25fdab13d467d564bee82905e9f0ae5c1bf2
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-03-02 09:32:12 -0500

    added introduction

---
+++ 01.Introduction/script.js
@@ -0,0 +1,23 @@
+function changeText() {
+    document.getElementById("text").innerHTML = "Hello JavaScript!";
+}
+
+function turnOn() {
+    document.getElementById("image").src = 'pic_bulbon.gif';
+}
+
+function turnOff() {
+    document.getElementById("image").src = 'pic_bulboff.gif';
+}
+
+function changeStyle() {
+    document.getElementById("text1").style.fontSize = '35px';
+}
+
+function hideText2() {
+    document.getElementById("text2").style.display = 'none';
+}
+
+function showText3() {
+    document.getElementById("text3").style.display = 'block';
+}
